d6e3c17b09bbd25a50a9d446c34cde6c7a78fcf8
Mariano <marianobitcode@gmail.com>
2018-09-25 08:41:34 -0500

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+module.exports = Phrase;
+
 // Reverses a string.
 // function reverse(string) {
 //   //return string.split("").reverse().join("");
@@ -14,9 +16,9 @@ function Phrase(content) {
 
   this.content = content;
 
-  this.processor = function(string) {
-    return string.toLowerCase();
-  }
+  // this.processor = function(string) {
+  //   return string.toLowerCase();
+  // }
 
   // Returns true if the phrase is a palindrome, false otherwise.
     this.palindrome = function palindrome() {
@@ -25,13 +27,36 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
     this.processedContent = function processedContent() {
-      return this.processor(content);
+      return this.content.match(/[a-z]/gi).join("").toLowerCase();  // 'g' global, 'i' upper and lower case
+      //return this.processor(this.letters());
+      // return this.letters().toLowerCase();
     }
 
   // Makes the phrase LOUDER.
     this.louder = function louder() {
       return this.content.toUpperCase();
     };
+
+    // Returns the letters in the content.
+    // For example:
+    //   new Phrase("Hello, world!").letters() === "Helloworld"
+
+    // this.letters = function letters() {
+      // let theLetters = [];
+      // const letterRegex = /[a-z]/i;
+                                    // /[a-zA-Z]/ === /[a-z]/i
+                                    // 'i' to make a case-insensitive match.
+      // 1 for (let i = 0; i < this.content.length; i++) {
+      // 1  let character = this.content.charAt(i);
+      // 2  Array.from(this.content).forEach(function(character) {
+      // 2    if (character.match(letterRegex)) {
+      // 2      theLetters.push(character);
+      // 2    }
+
+      //  return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+      //};
+      //return theLetters.join("");
+    //}
 }
 
 // Defines a TranslatedPhrase object.
---
+++ test/test.js
@@ -0,0 +1,46 @@
+// - We include two NPM modules using the require function.
+
+let assert = require("assert");       // an assertion library that lets us assert that things are true in our tests
+let Phrase = require("../index.js");  // the Phrase object itself:
+
+// we’ll use two functions from assert:
+// 'describe'  -> # takes a string and another function
+// 'it'        -> # takes a string and another function
+
+describe("Phrase", function() {                       // The object
+  describe("#palindrome", function() {                // Testing out one method of the object.
+    it("should return false for a non-palindrome", function() {
+      let nonPalindrome = new Phrase("apple");
+      assert(!nonPalindrome.palindrome());            // we’ve used assert to assert that "apple" should not be a palindrome
+    });
+
+    it("should return true for a plain palindrome", function() {
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+
+    it("should return true for a mixed-case palindrome", function() {
+      let mixedCase = new Phrase("RaceCar");
+      assert(mixedCase.palindrome());
+    });
+
+    it("should return true for a palindrome with punctuation", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+
+    // Adding a Pending Test: you accomplish it only writting the following
+    // it("should return true for a mixed-case palindrome");
+  });
+  /*
+  describe("#letters", function() {                            // Testing out another method
+    it("should return only letters", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      // When posible, use native assertions, like this one:
+      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+      // Instead of writting the test by yourself:
+      // assert(punctuatedPalindrome.letters() === "MadamImAdam");
+    });
+  });
+  */
+});
